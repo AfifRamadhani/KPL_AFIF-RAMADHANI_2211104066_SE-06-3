7f53e2e6b943537ee96fad6fba7a97a4f1bd9d58
AfifRamadhani <afiframadhani003@gmail.com>
2025-03-08 14:17:13 +0700

    oi

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variable-local-global.js
@@ -0,0 +1,12 @@
+let globalVarible = "Afif Ramadhani"
+
+function getMyIdentity() {
+    let localVariable = 2211104066
+
+    console.log(`Nama saya ${globalVarible} nim ${localVariable}`)
+}
+
+console.log(globalVarible)
+// console.log(localVariable)
+
+getMyIdentity()
---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variables.js
@@ -0,0 +1,15 @@
+var name = "Afif Ramadhani"
+const nim = 2211104066
+
+console.log(name)
+
+console.log("nama saya adalah " + name + " dan nim saya adalah " + nim)
+console.log(`Nama saya adalah ${name} dan nim saya adalah ${nim}`)
+
+console.log(`Nama saya adalah ${name}
+    nim saya adalah ${nim}`)
+
+
+var name = "ayam"
+
+let fullName = "Afif Ramadhani"
